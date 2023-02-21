990416754e5a4be14912a4633d1f1ba41bd0838d
Isabella <belladougherty@gmail.com>
2023-02-21 09:31:21 -0700

    Made it so each major portion was it's own function/sub and started the process of getting the overall system to let the user draw a card until the deck is empty then reshuffle. Currently working out errors in how to get the while loop to allow all cards to be drawn.

--- ShuffleTheDeck.vb
+++ ShuffleTheDeck.vb
@@ -6,21 +6,46 @@
 
 
 Imports System
+Imports System.ComponentModel.Design
+Imports System.Data.Common
 
 Module ShuffleTheDeck
     Sub Main()
         Dim deck(3, 12) As String
         Dim list(51) As String
         Dim shuffling(51) As String
+        Dim reshuffle As Boolean = False
+        Dim userInput As String = ""
         Dim temp As String
-        Dim iteration As Integer = 0
-        Dim random As New Random()
         Dim value As String
-        Dim row As Integer
-        Dim column As Integer
 
-        'Goes through and assigns each slot in the deck array as an individual card for each
-        'card in a standard 52 card deck
+        CreateDeck(deck)
+        Reassign(shuffling, deck)
+        Shuffle(shuffling)
+        ShuffledDeck(deck, shuffling)
+        'PrintDeck(deck)
+        Console.WriteLine("Press Enter to draw a card! Input anything at any time to quit")
+        userInput = Console.ReadLine()
+        While userInput = ""
+            If reshuffle = True Then
+                Console.WriteLine("Reshuffling the deck. Please hold...")
+                Console.WriteLine("...")
+                Shuffle(shuffling)
+                Console.WriteLine("...")
+                ShuffledDeck(deck, shuffling)
+                Console.WriteLine("...")
+                Console.WriteLine("Reshuffling complete!")
+            Else
+                DrawCard(deck, reshuffle)
+            End If
+            userInput = Console.ReadLine()
+        End While
+        Console.WriteLine("Have a good day!")
+    End Sub
+
+    'Goes through and assigns each slot in the deck array as an individual card for each
+    'card in a standard 52 card deck
+    Sub CreateDeck(deck(,))
         For i = 0 To 3
             For j = 0 To 12
                 Select Case j
@@ -63,32 +88,47 @@ Module ShuffleTheDeck
                 End Select
             Next
         Next
+    End Sub
 
-        'Iterates through the deck and assigns each value into the 1D array of shuffling
+    'Iterates through the deck and assigns each value into the 1D array of shuffling
+    Sub Reassign(shuffling(), deck(,))
+        Dim iteration As Integer = 0
         For i = 0 To 3
             For j = 0 To 12
-                'list(iteration) = deck(i, j)
                 shuffling(iteration) = deck(i, j)
                 iteration += 1
             Next
         Next
-        iteration = 0
+    End Sub
 
-        'Swaps values between shuffling
+    'Uses a simple swapping method to swap the values of the cards around
+    Function Shuffle(deck())
+        Dim value As String
+        Dim temp As String
+        Static count As Integer = 0
+        Dim random As New Random()
         For i = 0 To 51
             value = random.Next(0, 51)
-            temp = shuffling(i)
-            shuffling(i) = shuffling(value)
-            shuffling(value) = temp
+            temp = deck(count)
+            deck(count) = deck(value)
+            deck(value) = temp
         Next
-        'Reassigns the shuffling array to the deck array
+        Return deck
+    End Function
+
+    'Reassigns the shuffling array to the deck array
+    Sub ShuffledDeck(deck, shuffling)
+        Dim iteration As Integer = 0
         For i = 0 To 3
             For j = 0 To 12
                 deck(i, j) = shuffling(iteration)
                 iteration += 1
             Next
         Next
-        'Iterates through the deck array and prints it out nicely so the entire deck is visible
+    End Sub
+
+    'Iterates through the deck array and prints it out nicely so the entire deck is visible
+    Sub PrintDeck(deck(,))
         For i = 0 To 3
             For j = 0 To 12
                 Console.Write(deck(i, j) & " | ")
@@ -97,12 +137,45 @@ Module ShuffleTheDeck
             Console.WriteLine("-----------------------------------------------------------------")
             Console.WriteLine()
         Next
-        'Draws a random card
+    End Sub
+
+    'Draws a random card and makes sure said card has not already been pulled
+    Sub DrawCard(deck(,), reshuffle)
+        Static drawn(3, 12) As Boolean
+        Static count As Integer
+        Dim remake As Boolean = False
+        Dim row As Integer
+        Dim column As Integer
+        Dim random As New Random()
+
         row = random.Next(0, 3)
         column = random.Next(0, 12)
-        Console.WriteLine()
-        Console.WriteLine()
-        Console.WriteLine()
-        Console.WriteLine(deck(row, column))
+
+        Try
+            While drawn(row, column) = True Or count < 51
+                If count < 51 Then
+                    row = random.Next(0, 3)
+                    column = random.Next(0, 12)
+                    count += 1
+                Else
+                    remake = True
+                End If
+            End While
+        Catch ex As Exception
+            Console.WriteLine("FAILURE")
+        End Try
+        If remake = False Then
+            drawn(row, column) = True
+            Console.WriteLine(deck(row, column))
+        Else
+            Console.WriteLine("Remaking")
+            For i = 0 To 3
+                For j = 0 To 12
+                    drawn(i, j) = False
+                Next
+            Next
+            reshuffle = True
+        End If
     End Sub
+
 End Module
